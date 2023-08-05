1471fb15fa65f91b7f91884ef86c69ac5ba7db2a
1337rokudenashi <xyyylaufyyy1337@gmail.com>
2023-08-05 23:29:47 +0700

    pw2k3

---
+++ pw2k3/resources/views/anggota-kelompok.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app-bank-sampah')
+
+@section('title-page', 'Anggota Kelompok')
+
+@section('css')
+@endsection
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+
+            <div class="card">
+                <div class="card-header">Informasi Kelompok</div>
+                <div class="card-body">
+                    <p><strong>Nama Kelompok:</strong> PW 2 Kelompok 3</p>
+                    <p><strong>Anggota:</strong></p>
+                    <ul>
+                        <li>Antonius Sigid Priharsanto/210401070020</li>
+                        <li>Alpha Prima Galatheo Qallbu/210401010032</li>
+                        <li>Endang Yosa/210401010031</li>
+                        <li>Fandi Chriswantoro Putro/200401010074</li>
+                        <li>Farhan Rajareska Harahap/210401120001</li>
+                        <li>Syarifuddin/210401070017</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('js')
+@endsection
